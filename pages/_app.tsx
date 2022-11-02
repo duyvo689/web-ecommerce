2252f0c3cdcb86ed1f5c2f37257c74012eb5701b
duyvo689 <vothanhduy689@gmail.com>
2022-11-02 22:38:23 +0700

    init app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+              <Component {...pageProps} />
+              <Script src="../path/to/flowbite/dist/flowbite.js"></Script>
+          </>
 }
